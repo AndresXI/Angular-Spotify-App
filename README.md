11d9abaf41b35e8b06909179d0560923af8532f2
andres alcocer <andresalcocer7@yahoo.com>
2018-11-29 21:33:34 -0500

    readme file update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+# Angular Search Music App
 
+This was built with Angular and Bootstrap. It uses the spotify API to seach for artist.
--- server.js
+++ server.js
@@ -1,33 +1,72 @@
 //Install express server
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 var cors = require('cors');
-// configure server 
-const path = require('path'); 
-const http = require('http'); 
+// // configure server 
+// const path = require('path'); 
+// const http = require('http'); 
 
-const port = process.env.PORT || 3001; 
+// const port = process.env.PORT || 3001; 
+
+const path = require("path");
+const express = require("express");
+const app = express();
 
 
 
+// app.use(cors({
+//   credentials: true,
+//   origin: true
+// }))
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// });
+// app.options("*", cors());
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, application/x-www-form-urlencoded, Accept");
+//   next();
+// });
 
 
-app.use(cors({
-  credentials: true,
-  origin: true
-}))
+
+// var corsOptions = {
+//   origin: "http://localhost:3000",
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
+
+// app.get('/products/:id', cors(corsOptions), function (req, res, next) {
+//   res.json({ msg: 'This is CORS-enabled for only example.com.' })
+// })
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
+
+
+
+
+
+
+
 
 
 
 app.use(express.static(__dirname + '/dist/ng-spotify')); 
 
+// Send all requests to index.html
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/ng-spotify/index.html'));
+});
 
-const server = http.createServer(app); 
 
-server.listen(port, () => console.log("Corse Enaabled")); 
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
